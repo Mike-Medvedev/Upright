a32d2b3c6f357af54fadc9a69da0bec6efe21b37
Michael Medvedev <mmedvedev20@gmail.com>
2026-04-11 01:46:41 -0700

    implemented roboflow inference live sdk and detecting slouching

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,11 @@ import config from "@/config";
 import { z } from "zod";
 import cors from "cors";
 import helmet from "helmet";
-import { WebSocketServer } from "ws";
+import { createRequire } from "node:module";
 
-import { RTCPeerConnection, RTCSessionDescription, RTCIceCandidate, IceCandidate } from "werift";
+const require = createRequire(import.meta.url);
+const { InferenceHTTPClient } = require("@roboflow/inference-sdk");
+// import { InferenceHTTPClient } from "@roboflow/inference-sdk";
 
 const app = express();
 app.use(json());
@@ -27,107 +29,41 @@ router.get(
     res.json({ Hello: "World" });
   },
 );
-// const pc = new RTCPeerConnection({
-//   iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-// });
-// pc.onIceCandidate.subscribe((candidate) => {
-//   if (!candidate) {
-//     console.log("node ICE gathering complete");
-//     return;
-//   }
-//   console.log("SEND NODE CANDIDATE TO BROWSER:", JSON.stringify(candidate));
-// });
-// pc.onDataChannel.subscribe((channel) => {
-//   console.log("data channel:", channel.label);
 
-//   channel.onMessage.subscribe((msg) => {
-//     console.log("from browser:", msg.toString());
-//     channel.send("hello from node");
-//   });
-
-//   channel?.onopen?.subscribe(() => {
-//     console.log("data channel open");
-//   });
-// });
-
-// // If browser sends media tracks
-// pc.onTrack.subscribe((track) => {
-//   console.log("got remote track:", track.kind);
-// });
-
-// export async function handleBrowserOffer(offerSdp: string) {
-//   await pc.setRemoteDescription(new RTCSessionDescription(offerSdp, "offer"));
-
-//   const answer = await pc.createAnswer();
-//   await pc.setLocalDescription(answer);
-
-//   return pc.localDescription; // send this SDP back to browser
-// }
-
-// export async function addBrowserIceCandidate(candidateJson: any) {
-//   const candidate = RTCIceCandidate.fromJSON(candidateJson);
-//   if (candidate) {
-//     await pc.addIceCandidate(candidate);
-//   }
-// }
 app.use("/api/v1", router);
 app.use(swagger());
+
+app.post("/init-webrtc", async (req, res) => {
+  console.log("GOT REQUEST");
+  const { offer, wrtcParams } = req.body;
+
+  // API key stays secure on the server
+  const client = InferenceHTTPClient.init({
+    apiKey: config.ROBOFLOW_API_KEY,
+  });
+
+  const answer = await client.initializeWebrtcWorker({
+    offer,
+    workspaceName: wrtcParams.workspaceName,
+    workflowId: wrtcParams.workflowId,
+    config: {
+      streamOutputNames: wrtcParams.streamOutputNames,
+      dataOutputNames: wrtcParams.dataOutputNames,
+      workflowsParameters: wrtcParams.workflowsParameters,
+      requestedPlan: wrtcParams.requestedPlan,
+      requestedRegion: wrtcParams.requestedRegion,
+      // realtimeProcessing: wrtcParams.realtimeProcessing,
+      processingTimeout: wrtcParams.processingTimeout,
+    },
+  });
+
+  res.json(answer);
+});
 
 const server = app.listen(config.PORT, () => {
   console.log(`Server listening on port ${config.PORT}`);
 });
 
-const wss = new WebSocketServer({ server });
-wss.on("listening", () => console.log("Websocket listening..."));
-wss.on("connection", (ws, req) => {
-  console.log("WebSocket connected", req.url);
-
-  const pc = new RTCPeerConnection({
-    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-  });
-
-  pc.onconnectionstatechange = () => {
-    console.log(pc.connectionState);
-  };
-
-  pc.onTrack.subscribe((track) => {
-    console.log("PRINTING OUT TRACK", track);
-    track.onReceiveRtp.subscribe((packet) => console.log(packet));
-    // track.onReceiveRtcp.subscribe((rtcp) => console.log(rtcp));
-  });
-
-  pc.onIceCandidate.subscribe((candidate) => {
-    if (!candidate) return;
-    ws.send(JSON.stringify({ candidate }));
-  });
-
-  pc.onDataChannel.subscribe((channel) => {
-    channel.onMessage.subscribe((msg) => {
-      console.log("Video Frames", msg.toString());
-    });
-  });
-
-  ws.on("message", async (message) => {
-    let data: any;
-
-    try {
-      data = JSON.parse(message.toString());
-    } catch {
-      return;
-    }
-    console.log(data);
-    if ("offer" in data) {
-      await pc.setRemoteDescription(data.offer);
-
-      const answer = await pc.createAnswer();
-      await pc.setLocalDescription(answer);
-      console.log(answer);
-      ws.send(JSON.stringify({ answer }));
-    } else if ("candidate" in data) {
-      await pc.addIceCandidate(data.candidate);
-    } else console.log("NO OFFER OR CANDIDATE IN MESSAGE: ", data);
-  });
-});
 function shutdown(signal?: string) {
   console.log(`Gracefully shutting down due to ${signal}`);
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,107 +1,50 @@
 import "./App.css";
 import { useEffect, useRef } from "react";
-import { ws } from "@/websocket";
-// import { connectors, webrtc, streams } from "@roboflow/inference-sdk";
+import { connectors, webrtc, streams } from "@roboflow/inference-sdk";
 
-// // ⚠️ Use withApiKey for development only
-// // ⚠️ Do not use this in production, because it will expose your API key
-// // For production, use a backend proxy (see next section)
-// const connector = connectors.withApiKey("rf_IDQ7O7kE72NFYPEuBLQS27tkpsF3");
+const pc = async () => {
+  // Create a connector pointing to your backend proxy
+  const connector = connectors.withProxyUrl("/api/init-webrtc");
 
-// // Get camera stream
-// const stream = await streams.useCamera({
-//   video: {
-//     facingMode: { ideal: "environment" },
-//     width: { ideal: 640 },
-//     height: { ideal: 480 },
-//   },
-// });
+  // Start the stream
+  const connection = await webrtc.useStream({
+    source: await streams.useCamera({ video: true, audio: false }),
+    connector: connector,
+    wrtcParams: {
+      workspaceName: "gilded-6esmg",
+      workflowId: "custom-workflow-5",
+      streamOutputNames: ["output_image"],
+      dataOutputNames: ["posture_height", "status"],
+      processingTimeout: 3600,
+      requestedPlan: "webrtc-gpu-medium", // Options: webrtc-gpu-small, webrtc-gpu-medium, webrtc-gpu-large
+      requestedRegion: "us", // Options: us, eu, ap,
+      workflowsParameters: {
+        is_calibrating: false,
+        baseline_height: 100,
+        threshold_ratio: 0.8,
+      },
+    },
+    onData: (data) => console.log("Predictions:", data),
+  });
 
-// // Start WebRTC connection
-// const connection = await webrtc.useStream({
-//   source: stream,
-//   connector,
-//   wrtcParams: {
-//     workspaceName: "your-workspace",
-//     workflowId: "your-workflow",
-//     imageInputName: "image",
-//     streamOutputNames: ["output"],
-//     dataOutputNames: ["predictions"],
-//   },
-//   onData: (data) => {
-//     console.log("Inference results:", data);
-//   },
-// });
-
-// // Display processed video
-// const videoElement = document.getElementById("video");
-// videoElement.srcObject = await connection.remoteStream();
-
-// // Clean up when done
-// await connection.cleanup();
+  // Display the processed video
+  const remoteStream = await connection.remoteStream();
+  const videoElement = document.querySelector("video");
+  videoElement.srcObject = remoteStream;
+};
 
 export default function App() {
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  const iceCandidateQueue = useRef<RTCIceCandidateInit[]>([]);
-  const isRTCOfferAnswered = useRef<boolean>(false);
 
   useEffect(() => {
     (async () => {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-      });
-      console.log("PRINTING TRACKS", stream.getTracks());
-      videoRef.current.srcObject = stream;
-      const config = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
-      const rtcConnection = new RTCPeerConnection(config);
-      for (const track of stream.getTracks()) {
-        rtcConnection.addTrack(track, stream);
-      }
-      const channel = rtcConnection.createDataChannel("chat");
-
-      channel.onopen = () => {
-        console.log("data channel open");
-        channel.send("hello");
-      };
-
-      channel.onmessage = (event) => {
-        console.log("message from backend:", event.data);
-      };
-      rtcConnection.onicecandidate = async (event) => {
-        console.log(" GATHERED AND SENDING ICE CANDIDATE");
-        if (!event.candidate) return;
-        ws.send(JSON.stringify({ candidate: event.candidate }));
-      };
-
-      rtcConnection.onnegotiationneeded = async (event) => {
-        console.log("PRINTING EVENT OF NEGOTATION", event);
-        const offer = await rtcConnection.createOffer();
-        await rtcConnection.setLocalDescription(offer);
-        ws.send(JSON.stringify({ offer }));
-      };
-
-      ws.onmessage = async (data) => {
-        console.log("RECIEVED WEBSOCKET MESSAGE", data);
-        const message = JSON.parse(data.data);
-        console.log("RAW MESSAGE", message);
-
-        if ("answer" in message) {
-          await rtcConnection.setRemoteDescription(message.answer);
-          isRTCOfferAnswered.current = true;
-          iceCandidateQueue.current.forEach((candidate) =>
-            rtcConnection.addIceCandidate(candidate),
-          );
-        } else if ("candidate" in message) {
-          if (isRTCOfferAnswered.current) await rtcConnection.addIceCandidate(message.candidate);
-          else iceCandidateQueue.current.push(message.candidate);
-        } else console.log("recieved unknown message");
-      };
+      await pc();
     })();
-  });
+  }, []);
+
   return (
     <div className="app-container">
       <main className="main-container">
-        <button style={{ padding: "1rem" }}></button>
         {<video autoPlay ref={videoRef} width={500} height={500}></video>}
       </main>
     </div>
