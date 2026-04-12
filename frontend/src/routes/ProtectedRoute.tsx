055d56a0a25eb7f514c67ff3ed9359d91d63a2cc
Michael Medvedev <mmedvedev20@gmail.com>
2026-04-12 15:18:10 -0700

    fixed app loading view flicker on page refresh

--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -1,19 +1,19 @@
 import { Navigate, Outlet, useLocation } from "react-router";
 import { useAuth } from "@/infra/auth/auth.context";
-import { AppLoadingView } from "@/theme/components/AppLoadingView/AppLoadingView";
 
 export function ProtectedRoute() {
   const location = useLocation();
   const { status } = useAuth();
 
-  if (status === "loading") {
-    return (
-      <AppLoadingView
-        description="We are restoring your session before loading the app."
-        title="Checking your account"
-      />
-    );
-  }
+  if (status === "loading") return null; //dont show anytthing so refreshes dont flicker ap loading view
+  // if (status === "loading") {
+  //   return (
+  //     <AppLoadingView
+  //       description="We are restoring your session before loading the app."
+  //       title="Checking your account"
+  //     />
+  //   );
+  // }
 
   if (status !== "authenticated") {
     return <Navigate replace state={{ from: location }} to="/login" />;
