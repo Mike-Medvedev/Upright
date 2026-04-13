99144d02d69ac77e43f5cbca4bb2baac9292e4ed
Michael Medvedev <mmedvedev20@gmail.com>
2026-04-12 17:17:32 -0700

    draw on canvas on local stream

--- frontend/src/features/monitoring/components/CameraPreview.tsx
+++ frontend/src/features/monitoring/components/CameraPreview.tsx
@@ -2,11 +2,20 @@ import { Paper, Loader, Text } from "@mantine/core";
 import useLocalCamera from "@/features/monitoring/hooks/useLocalCamera";
 import useInferencePipeline from "@/features/monitoring/hooks/useInferencePipeline";
 import { InferenceOverlay } from "@/features/monitoring/components/InferenceOverlay";
+import { useRef } from "react";
 export function CameraPreview() {
   const { cameraStream, videoRef, isLoading, error } = useLocalCamera();
-
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const { isConnected } = useInferencePipeline(cameraStream, (data) => {
     console.log("Prediction:", data);
+    if (!canvasRef.current) return;
+    const ctx = canvasRef.current.getContext("2d");
+    if (!ctx) return;
+
+    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    ctx.font = "24px Inter";
+    ctx.fillStyle = data.serialized_output_data.status === "Good Posture" ? "green" : "red";
+    ctx.fillText(data.serialized_output_data.status, 20, 40);
   });
 
   if (isLoading) return <Loader />;
@@ -23,6 +32,10 @@ export function CameraPreview() {
         autoPlay
         muted
         style={{ width: "100%", height: "100%", objectFit: "cover", display: "block" }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{ position: "absolute", inset: 0, width: "100%", height: "100%" }}
       />
       <InferenceOverlay isConnected={isConnected} />
     </Paper>
